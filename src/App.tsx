8845425de12e29bf161543e26aa6822cecac28ca
Ypisds <thrbritto4@gmail.com>
2026-03-03 10:19:36 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { Header } from './component/Header/Header.tsx'
+
+function App() {
+
+  return (
+    <>
+      <h1> Hello World</h1>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/Header/Header.tsx
@@ -0,0 +1,8 @@
+
+
+export function Header(){
+
+    return (
+        <h1>Sou um header</h1>
+    )
+}
